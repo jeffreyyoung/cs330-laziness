2544020284b0e0d26e1d4c64319765abb3f593a2
Keith Halterman <keithhalterman@gmail.com>
2015-02-25 10:38:34 -0700

    Added Take-While Test Method

--- laziness.rkt
+++ laziness.rkt
@@ -1,17 +1,5 @@
 #lang lazy
 
-
-(define print-only-errors #t)
-
-;test function, passes if the parameters l r are equal, otherwise fails
-(define (test l r)
-  (if (equal? l r)
-      (if print-only-errors
-          (printf "meh")
-          (printf "Test Passed~n"))
-      (printf "Test Failed.~nActual:   ~S ~nExpected: ~S~n" l r)))
-
-
 ;; returns the prefix of l such that for all elements p returns true
 (define (take-while p l)
    (if (empty? l)
@@ -20,8 +8,27 @@
            (cons (first l) (take-while p (rest l)))
            empty
            )))
-   
 
-(display (take-while (lambda (n) (< n 5)) (list 1 2 3 4 5 1 2)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;TESTING METHODS
+
+(define print-only-errors #f)
+
+;;test function, passes if the parameters l r are equal, otherwise fails
+(define (test l r)
+  (if (equal? l r)
+      (if print-only-errors
+          ()
+          (printf "Test Passed~n"))
+      (printf "Test Failed.~nActual:   ~S ~nExpected: ~S~n" l r)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;TEST TAKE_WHILE
+(test (take-while odd? (list 1 3 4))
+      (list 1 3))
+
+(test (take-while (lambda (n) (< n 5)) (list 1 2 3 4 5 1 2)) 
+      (list 1 2 3 4))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
