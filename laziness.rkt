f28d95b7c9da13628cad9b0325f435129ed36bf3
Jeffrey Young <jeffreeyoung@yahoo.com>
2015-02-25 10:34:48 -0700

    adding take-while

--- laziness.rkt
+++ laziness.rkt
@@ -7,11 +7,21 @@
 (define (test l r)
   (if (equal? l r)
       (if print-only-errors
-          ()
+          (printf "meh")
           (printf "Test Passed~n"))
       (printf "Test Failed.~nActual:   ~S ~nExpected: ~S~n" l r)))
 
-(printf "hello world")
 
+;; returns the prefix of l such that for all elements p returns true
+(define (take-while p l)
+   (if (empty? l)
+       empty
+       (if (p (first l))
+           (cons (first l) (take-while p (rest l)))
+           empty
+           )))
+   
+
+(display (take-while (lambda (n) (< n 5)) (list 1 2 3 4 5 1 2)))
 
 
